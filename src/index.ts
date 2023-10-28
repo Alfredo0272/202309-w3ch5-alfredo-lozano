3500838f32b95345d5056c17576315ea20917770
Alfredo Lozano <alfredo.realizador@gmail.com>
2023-10-28 09:45:20 +0200

    Create pokelist

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-new */
 import { Header } from './component/header';
 import { Main } from './component/main';
+import { Navigator } from './component/navigator';
 
 new Header('#app');
 new Main('#app');
+new Navigator('#app');
